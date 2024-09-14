5ed952b8b892ae12776d03bd2770fc9e142150d5
fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>
2024-09-14 12:52:21 +0800

    Signed-off-by: fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>

--- s1/w1.1.py
+++ s1/w1.1.py
@@ -1,4 +1,4 @@
-radius = 25
-area = 3.1415 * radius * radius
-print(area)
-print("{:.2f}".format(area))
+radius = 25 #圆的半径为25
+area = 3.1415 * radius * radius #计算圆的面积
+print(area) #输出圆的面积
+print("{:.2f}".format(area)) #输出圆的面积，保留两位小数
