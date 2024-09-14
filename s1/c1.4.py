f826c585e25c6f7082c70e5e98e36ef01b997afb
fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>
2024-09-14 13:48:17 +0800

    Signed-off-by: fanqie <12553846+fanqiejiayou1024@user.noreply.gitee.com>

--- s1/c1.4.py
+++ s1/c1.4.py
@@ -1,5 +1,5 @@
-sum,tmp = 0,1
-for i in range(1,11):
-    tmp = tmp * i
-    sum += tmp
-print("运算的结果是：{}".format(sum))
+sum,tmp = 0,1 # 初始化sum tmp
+for i in range(1,11): # 循环
+    tmp = tmp * i # 计算tmp
+    sum += tmp # 计算sum
+print("运算的结果是：{}".format(sum)) # 输出sum
